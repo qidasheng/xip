0705a65fe8a0c0029753380c08cca533bb12d8e7
qidasheng <qsf.zzia1@hotmail.com>
2015-02-15 14:46:39 +0800

    纯真数据库路径参数化

--- xip.sh
+++ xip.sh
@@ -1,3 +1,4 @@
+gcc -Wall -c cjson/cJSON.c  -lm
 gcc -Wall -c libqqwry/qqwry.c
 gcc -Wall -c xip.c   -levent -L /usr/local/libevent2/lib/ -I /usr/local/libevent2/include/
 gcc -Wall -o xip qqwry.o cJSON.o xip.o   -levent -L /usr/local/libevent2/lib/ -I /usr/local/libevent2/include/ -lm -liconv
